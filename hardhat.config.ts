bf2982ad4022fe7d7db58527c481e1b2568f098e
intosKai <ivanoff.pg@ya.ru>
2024-05-16 17:14:09 +0300

    use vars

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,29 +1,35 @@
-import { HardhatUserConfig } from "hardhat/config";
+import { HardhatUserConfig, vars } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@openzeppelin/hardhat-upgrades";
-
-import "dotenv";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
   networks: {
     holesky: {
       loggingEnabled: true,
-      url: process.env.HOLESKY_RPC || "https://rpc.ankr.com/eth_holesky",
+      url: vars.get("HOLESKY_RPC", "https://rpc.ankr.com/eth_holesky"),
       chainId: 17_000,
-      accounts: [
-        process.env.DEPLOYER_SK ||
-          "77f0ea910db71438400c072581e7e29c579107725f0f97ae07c56aaecac39ea5",
-      ],
+      accounts: [vars.get("TESTNET_DEPLOYER_SK")],
       timeout: 90_000,
       gas: 8_000_000,
       gasMultiplier: 1.5,
     },
+    mainnet: {
+      loggingEnabled: true,
+      url: vars.get("MAINNET_RPC", "https://rpc.ankr.com/eth"),
+      chainId: 1,
+      accounts: [vars.get("DEPLOYER_SK")],
+      timeout: 90_000,
+      gas: 8_000_000,
+      gasMultiplier: 1,
+    },
   },
   etherscan: {
     apiKey: {
-      holesky:
-        process.env.ETHERSCAN_API_KEY || "PP5CDPZBG6AF6FBGE9CJNYGCRYXYN549M1",
+      holesky: vars.get(
+        "ETHERSCAN_API_KEY",
+        "PP5CDPZBG6AF6FBGE9CJNYGCRYXYN549M1"
+      ),
     },
   },
 };
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,6 +1,10 @@
 import { ethers, upgrades } from "hardhat";
+import { vars } from "hardhat/config";
 
-const consortiumAddress = "0x1820b9218cb2D9a3790EDe3b5F20851BEc8971B0";
+const consortiumAddress = vars.get(
+  "CONSORTIUM_ADDRESS",
+  "0x1820b9218cb2D9a3790EDe3b5F20851BEc8971B0"
+);
 
 async function main() {
   const accounts = await ethers.getSigners();
