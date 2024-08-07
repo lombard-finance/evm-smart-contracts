ec60a6e431d3ffd210c5b98517bd78000d34777f
hashxtree <145032760+hashxtree@users.noreply.github.com>
2024-08-07 11:14:01 +0300

    enforce pragma

--- contracts/libs/EIP1271SignatureUtils.sol
+++ contracts/libs/EIP1271SignatureUtils.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.24;
+pragma solidity 0.8.24;
 
 import "@openzeppelin/contracts/interfaces/IERC1271.sol";
 import "@openzeppelin/contracts/utils/Address.sol";
