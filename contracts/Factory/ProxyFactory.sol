1ba0b9ecf77b6f2a64e4119880e4dff211367d19
Lazaro Raul Iglesias Vera <riglesiasvera@gmail.com>
2024-10-04 20:15:25 +0200

    feat: ProxyFactory contract

---
+++ contracts/Factory/ProxyFactory.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {CREATE3} from "solmate/src/utils/CREATE3.sol";
+import {TransparentUpgradeableProxy} from "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
+
+contract ProxyFactory {
+    function createTransparentProxy(address implementation, address admin, bytes memory data, bytes32 salt) public returns (address) {
+        bytes memory bytecode = abi.encodePacked(
+            type(TransparentUpgradeableProxy).creationCode,
+            abi.encode(implementation, admin, data)
+        );
+
+        address proxy = CREATE3.deploy(salt, bytecode, 0);
+        return proxy;
+    }
+
+    function getDeployed(bytes32 salt) public view returns (address) {
+        return CREATE3.getDeployed(salt);
+    }
+}
