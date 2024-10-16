0a86a22ebdbf7857d6108fdc6fa1fcdca3de9221
Lazaro Raul Iglesias Vera <riglesiasvera@gmail.com>
2024-10-16 12:08:34 -0400

    fix: Revert deploy-timelock changes

--- scripts/deploy-timelock.ts
+++ scripts/deploy-timelock.ts
@@ -1,48 +1,5 @@
-import {getProxyFactoryAt, getProxySalt, verify} from "./helpers";
-import { task } from "hardhat/config";
-import {DEFAULT_PROXY_FACTORY} from "./helpers/constants";
-
-task("deploy-timelock", "Deploys the LombardTimeLock contract via create3")
-  .addParam("ledgerNetwork", "The network name of ledger", "mainnet")
-  .addParam("admin", "The address of the owner")
-  .addParam("minDelay", "Minimum delay between proposal and execution")
-  .addVariadicPositionalParam("proposers", "The list of proposers")
-  .addVariadicPositionalParam("executors", "The list of executors")
-  .addParam("proxyFactoryAddr", "The ProxyFactory address", DEFAULT_PROXY_FACTORY)
-  .setAction(async (taskArgs, hre) => {
-
-    const { ledgerNetwork, admin, proxyFactoryAddr, minDelay, proposers, executors } = taskArgs;
-    const { ethers, run, upgrades } = hre;
-
-    const factory = await getProxyFactoryAt(ethers, proxyFactoryAddr);
-    const saltHash = getProxySalt(ethers, ledgerNetwork, "LombardTimelock");
-
-    const timelockImpl = await ethers.deployContract("LombardTimelock");
-    await timelockImpl.waitForDeployment();
-
-    const data = timelockImpl.interface.encodeFunctionData("initialize", [minDelay, proposers, executors]);
-
-    const tx = await factory.createTransparentProxy(await timelockImpl.getAddress(), admin, data, saltHash);
-    await tx.wait();
-
-    const proxy = await factory.getDeployed(saltHash);
-    console.log("Proxy address:", proxy);
-
-    const proxyAdmin = await upgrades.erc1967.getAdminAddress(proxy);
-    console.log("Proxy admin:", proxyAdmin);
-
-    await verify(run, await factory.getAddress());
-    await verify(run, await timelockImpl.getAddress());
-    await verify(run, proxyAdmin, {
-      contract: "@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol:ProxyAdmin",
-      constructorArguments: [admin],
-    });
-    await verify(run, proxy, {
-      contract: "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol:TransparentUpgradeableProxy",
-      constructorArguments: [await timelockImpl.getAddress(), admin, data],
-    });
-  });
-
+import { ethers, network, run } from "hardhat";
+import { getAddresses, sleep } from "./helpers";
 
 async function main() {
   const addresses = getAddresses(network.name);
