e088100f5f45bebb73736e68b7f98f7bdedbeee2
hashxtree <145032760+hashxtree@users.noreply.github.com>
2024-10-18 15:06:31 +0300

    comment token pool

--- scripts/deployments/token-pool.ts
+++ scripts/deployments/token-pool.ts
@@ -1,22 +1,31 @@
-import { task } from "hardhat/config"; 
+import { task } from 'hardhat/config';
 
 /*
  * After deployment:
  * 1. Configure token pool
  */
-task("deploy-token-pool", "Deploys chainlink TokenPool contract")
-  .addParam("router", "The chainlink ccip router")
-  .addParam("lbtc", "The address of the LBTC contract / token to be used in the pool")
-  .addParam("adapter", "The address of the adapter")
-  .addParam("rmn", "The address of the RmnProxy")
-  .addParam("router", "The address of the chainlink ccip router")
-  .addVariadicPositionalParam("allowlist", "The list of addresses allowed to bridge")
-  .setAction(async (taskArgs, hre) => {
-    const { lbtc, rmn, router, adapter, allowlist } = taskArgs;
+task('deploy-ccip-token-pool', 'Deploys chainlink TokenPool contract')
+    .addParam('router', 'The chainlink ccip router')
+    .addParam(
+        'lbtc',
+        'The address of the LBTC contract / token to be used in the pool'
+    )
+    .addParam('adapter', 'The address of the adapter')
+    .addParam('rmn', 'The address of the RmnProxy')
+    .addParam('router', 'The address of the chainlink ccip router')
+    .addVariadicPositionalParam(
+        'allowlist',
+        'The list of addresses allowed to bridge'
+    )
+    .setAction(async (taskArgs, hre) => {
+        const { lbtc, rmn, router, adapter, allowlist } = taskArgs;
 
-    const pool = await hre.ethers.deployContract(
-      "TokenPool",
-      [adapter, lbtc, allowlist, rmn, router]
-    );
-    console.log("TokenPool:", await pool.getAddress());
-  });
+        const pool = await hre.ethers.deployContract('TokenPool', [
+            adapter,
+            lbtc,
+            allowlist,
+            rmn,
+            router,
+        ]);
+        console.log('TokenPool:', await pool.getAddress());
+    });
