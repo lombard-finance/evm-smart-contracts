2a3fe59d0d1f28a7804dbadf571c21a318426b52
Jules de Smit <julesdesmit@gmail.com>
2024-11-26 13:20:08 +0100

    Add stake and bake skeleton

---
+++ contracts/stakeAndBake/stakeAndBake.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+/**
+ * @title Convenience contract for users who wish to 
+ * stake their BTC and deposit it in a vault immediately
+ * @author Lombard.Finance
+ * @notice This contract is a part of the Lombard.Finance protocol
+ */
+contract StakeAndBake {
+
+}
