3b3a75e47e5895bfa1aa58998787e0ff621d5c2c
hashxtree <145032760+hashxtree@users.noreply.github.com>
2024-12-03 18:14:20 +0300

    default admin

--- scripts/lbtc.ts
+++ scripts/lbtc.ts
@@ -1,6 +1,6 @@
 import { task } from "hardhat/config";
-import {getProxyFactoryAt, getProxySalt, verify} from "./helpers";
-import {DEFAULT_PROXY_FACTORY} from "./helpers/constants";
+import { getProxyFactoryAt, getProxySalt, verify } from "./helpers";
+import { DEFAULT_PROXY_FACTORY } from "./helpers/constants";
 
 /*
  * After deployment:
@@ -9,16 +9,33 @@ import {DEFAULT_PROXY_FACTORY} from "./helpers/constants";
  * 3. Set pauser
  */
 
-
 task("deploy-lbtc", "Deploys the LBTC contract")
   .addParam("ledgerNetwork", "The network name of ledger", "mainnet")
   .addParam("consortium", "The address of LombardConsortium")
   .addParam("burnCommission", "The burn commission")
-  .addParam("admin", "The owner of the proxy")
-  .addParam("proxyFactoryAddr", "The ProxyFactory address", DEFAULT_PROXY_FACTORY)
+  .addParam("admin", "The owner of the proxy", "self")
+  .addParam(
+    "proxyFactoryAddr",
+    "The ProxyFactory address",
+    DEFAULT_PROXY_FACTORY
+  )
   .setAction(async (taskArgs, hre, network) => {
-    const { ledgerNetwork, consortium, burnCommission, testEnv, admin, proxyFactoryAddr } = taskArgs;
+    const {
+      ledgerNetwork,
+      consortium,
+      burnCommission,
+      testEnv,
+      admin,
+      proxyFactoryAddr,
+    } = taskArgs;
     const { ethers, run, upgrades } = hre;
+
+    const [signer] = await hre.ethers.getSigners();
+    let owner = await signer.getAddress();
+
+    if (hre.ethers.isAddress(admin)) {
+      owner = admin;
+    }
 
     const factory = await getProxyFactoryAt(ethers, proxyFactoryAddr);
     const saltHash = getProxySalt(ethers, ledgerNetwork, "LBTC");
@@ -26,26 +43,37 @@ task("deploy-lbtc", "Deploys the LBTC contract")
     const lbtcImpl = await ethers.deployContract("LBTC");
     await lbtcImpl.waitForDeployment();
 
-    const data = lbtcImpl.interface.encodeFunctionData("initialize", [consortium, burnCommission, admin]);
+    const data = lbtcImpl.interface.encodeFunctionData("initialize", [
+      consortium,
+      burnCommission,
+      owner,
+    ]);
 
-    const tx = await factory.createTransparentProxy(await lbtcImpl.getAddress(), admin, data, saltHash);
+    const tx = await factory.createTransparentProxy(
+      await lbtcImpl.getAddress(),
+      owner,
+      data,
+      saltHash
+    );
     await tx.wait();
-    
+
     const proxy = await factory.getDeployed(saltHash);
     console.log("Proxy address:", proxy);
-    
+
     const proxyAdmin = await upgrades.erc1967.getAdminAddress(proxy);
     console.log("Proxy admin:", proxyAdmin);
 
     await verify(run, await factory.getAddress());
     await verify(run, await lbtcImpl.getAddress());
     await verify(run, proxyAdmin, {
-      contract: "@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol:ProxyAdmin",
-      constructorArguments: [admin],
+      contract:
+        "@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol:ProxyAdmin",
+      constructorArguments: [owner],
     });
     await verify(run, proxy, {
-      contract: "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol:TransparentUpgradeableProxy",
-      constructorArguments: [await lbtcImpl.getAddress(), admin, data],
+      contract:
+        "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol:TransparentUpgradeableProxy",
+      constructorArguments: [await lbtcImpl.getAddress(), owner, data],
     });
 
     // reinitialize
