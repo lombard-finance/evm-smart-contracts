b51fa2242c621b35d48448cceb7ea13c225ba2c1
Jules de Smit <julesdesmit@gmail.com>
2024-12-12 17:51:28 +0100

    Fix teller depositor deployment script

--- scripts/deployments/teller-depositor.ts
+++ scripts/deployments/teller-depositor.ts
@@ -1,21 +1,16 @@
 import { task } from 'hardhat/config';
-import { DEFAULT_PROXY_FACTORY } from '../helpers/constants';
 import { create3 } from '../helpers/create3Deployment';
+import { sleep } from '../helpers';
 
 task(
     'deploy-teller-depositor',
     'Deploys the TellerWithMultiAssetSupportDepositor contract'
 )
     .addParam('ledgerNetwork', 'The network name of ledger', 'mainnet')
-    .addParam(
-        'proxyFactoryAddr',
-        'The ProxyFactory address',
-        DEFAULT_PROXY_FACTORY
-    )
     .setAction(async (taskArgs, hre, network) => {
         const { ethers } = hre;
 
-        const { ledgerNetwork, proxyFactoryAddr } = taskArgs;
+        const { ledgerNetwork } = taskArgs;
 
         const args = [];
 
@@ -24,4 +19,19 @@ task(
             args
         );
         await adapter.waitForDeployment();
+
+        console.log(`Deployment address is ${await adapter.getAddress()}`);
+        console.log(`Going to verify...`);
+
+        await sleep(12_000);
+        try {
+            await run('verify:verify', {
+                address: await adapter.getAddress(),
+                contract:
+                    'contracts/stakeAndBake/depositor/TellerWithMultiAssetSupportDepositor.sol:TellerWithMultiAssetSupportDepositor',
+                args,
+            });
+        } catch (e) {
+            console.error(`Verification failed: ${e}`);
+        }
     });
