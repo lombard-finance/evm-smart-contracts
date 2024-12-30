102e8822a583b16ead39d871a8072ae28270858c
Antonio Russo <antonio@lombard.finance>
2024-12-30 16:00:52 +0100

    fix: blacklist typos

--- contracts/blacklist/DepositNotarizationBlacklist.sol
+++ contracts/blacklist/DepositNotarizationBlacklist.sol
@@ -13,10 +13,10 @@ contract DepositNotarizationBlacklist is
     IDepositNotarizationBlacklist,
     AccessControlUpgradeable
 {
-    bytes32 public constant ADD_BLOCKLIST_ROLE =
-        keccak256("ADD_BLOCKLIST_ROLE");
-    bytes32 public constant REMOVE_BLOCKLIST_ROLE =
-        keccak256("REMOVE_BLOCKLIST_ROLE");
+    bytes32 public constant ADD_BLACKLIST_ROLE =
+        keccak256("ADD_BLACKLIST_ROLE");
+    bytes32 public constant REMOVE_BLACKLIST_ROLE =
+        keccak256("REMOVE_BLACKLIST_ROLE");
 
     mapping(bytes32 => mapping(uint32 => bool)) internal _blacklist;
 
@@ -58,7 +58,7 @@ contract DepositNotarizationBlacklist is
     function addToBlacklist(
         bytes32 txId,
         uint32[] calldata vouts
-    ) external onlyRole(ADD_BLOCKLIST_ROLE) {
+    ) external onlyRole(ADD_BLACKLIST_ROLE) {
         for (uint i = 0; i < vouts.length; i++) {
             _blacklist[txId][vouts[i]] = true;
             emit Blacklisted(txId, vouts[i], msg.sender);
@@ -73,7 +73,7 @@ contract DepositNotarizationBlacklist is
     function removeFromBlacklist(
         bytes32 txId,
         uint32[] calldata vouts
-    ) external onlyRole(REMOVE_BLOCKLIST_ROLE) {
+    ) external onlyRole(REMOVE_BLACKLIST_ROLE) {
         for (uint i = 0; i < vouts.length; i++) {
             _blacklist[txId][vouts[i]] = false;
             emit Cleared(txId, vouts[i], msg.sender);
