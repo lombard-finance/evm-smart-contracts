6de315da0e9222078ffe70fcc0e9b6bc10023e8d
Jules de Smit <julesdesmit@gmail.com>
2025-01-23 20:49:57 +0400

    Use veda vault premium calculation

--- scripts/deployments/teller-mock.ts
+++ scripts/deployments/teller-mock.ts
@@ -1,17 +1,22 @@
 import { task } from 'hardhat/config';
 import { verify } from '../helpers';
 
-task('deploy-teller-mock', 'Deploys the Teller Mock contract').setAction(
-    async (taskArgs, hre) => {
+task('deploy-teller-mock', 'Deploys the Teller Mock contract')
+    .addParam('lbtc', 'The address of the LBTC contract')
+    .setAction(async (taskArgs, hre) => {
         const { ethers, run } = hre;
 
-        const deployment = await ethers.deployContract(
-            'TellerWithMultiAssetSupportMock'
+        const { lbtc } = taskArgs;
+
+        const deployment = await hre.ethers.deployContract(
+            'TellerWithMultiAssetSupportMock',
+            [lbtc]
         );
         await deployment.waitForDeployment();
 
         console.log(`Deployment address is ${await deployment.getAddress()}`);
 
-        await verify(run, await deployment.getAddress());
-    }
-);
+        await verify(hre.run, await deployment.getAddress(), {
+            constructorArguments: [lbtc],
+        });
+    });
