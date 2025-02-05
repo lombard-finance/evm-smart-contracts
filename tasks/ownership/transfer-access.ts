842b563d9c6c244894a8a91b1d964d3461c945e2
Jules de Smit <julesdesmit@gmail.com>
2025-02-05 13:11:11 +0400

    Update bytes encoding for access control transfer

--- tasks/ownership/transfer-access.ts
+++ tasks/ownership/transfer-access.ts
@@ -9,12 +9,18 @@ export async function transferAccessControl(
     const { owner, target } = taskArgs;
 
     const contract = await ethers.getContractAt('IAccessControl', target);
-    const tx = await contract.grantRole(0x00, owner);
+    const tx = await contract.grantRole(
+        ethers.encodeBytes32String('0x00'),
+        owner
+    );
     await tx.wait(2);
     console.log(`grant tx hash: ${tx.hash}`);
 
     const [signer] = await ethers.getSigners();
-    const tx2 = await contract.revokeRole(0x00, await signer.getAddress());
+    const tx2 = await contract.revokeRole(
+        ethers.encodeBytes32String('0x00'),
+        await signer.getAddress()
+    );
     await tx2.wait(2);
     console.log(`revoke tx hash: ${tx2.hash}`);
 }
