34a613a747afb8c0048429edf122b2aaf3943f74
Jules de Smit <julesdesmit@gmail.com>
2025-04-16 17:09:25 +0400

    Add tests for kiln vault

---
+++ contracts/mock/KilnVaultMock.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+import {ERC4626} from "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract KilnVaultMock is ERC4626 {
+    constructor(IERC20 asset_) ERC4626(asset_) ERC20("Kiln LBTC", "kLBTC") {}
+}
