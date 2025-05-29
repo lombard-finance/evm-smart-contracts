69472672ce47463ba2cd3ed92065f1ef4826db03
555-andrew <andreyb@lombard.finance>
2025-05-29 17:51:22 +0300

    Update contracts/BARD/BARD.sol
    Co-authored-by: Peter <145032760+hashxtree@users.noreply.github.com>

--- contracts/BARD/BARD.sol
+++ contracts/BARD/BARD.sol
@@ -1,8 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.24;
 
-import {Ownable2Step} from "@openzeppelin/contracts/access/Ownable2Step.sol";
-import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {Ownable, Ownable2Step} from "@openzeppelin/contracts/access/Ownable2Step.sol";
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {ERC20Burnable} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
