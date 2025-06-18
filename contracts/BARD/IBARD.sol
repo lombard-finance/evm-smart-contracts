81311585ffacda8058f76bc19ba6f99be5ec90fc
Andrey B-L <andreyb@lombard.finance>
2025-06-18 12:10:23 +0400

    BARD audit fixes 2

--- contracts/BARD/IBARD.sol
+++ contracts/BARD/IBARD.sol
@@ -2,8 +2,6 @@
 pragma solidity 0.8.24;
 
 interface IBARD {
-    error ZeroAddressException();
-
     error MintWaitPeriodNotClosed(uint256 timeToWait);
 
     error MaxInflationExceeded(uint256 maxAllowedAmount);
